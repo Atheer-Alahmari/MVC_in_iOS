e8d63e15a672a55ffbd791f4d14eb821f88858d4
Atheer-Alahmari <Atheer.a.alahmari@gmail.com>
2021-12-23 18:30:09 +0300

    MVC iOS

---
+++ MVC_in_iOS/GETFilms_TableViewController.swift
@@ -0,0 +1,70 @@
+//
+//  GETFilms_TableViewController.swift
+//  MVC_in_iOS
+//
+//  Created by Atheer Alahmari on 19/05/1443 AH.
+//
+
+import UIKit
+class GETFilms_TableViewController: UITableViewController {
+    // Hardcoded data for now
+    var films :[String] = []
+    
+    override func viewDidLoad()  {
+        super.viewDidLoad()
+       getFilmsApi()
+    }
+    
+
+    func getFilmsApi(){
+        ApiManager.getAllFilms(completionHandler:{ data, response, error in
+        do {
+            if let jsonResult = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary {
+                if let results = jsonResult["results"] {
+                    
+                    let resultsArray = results as! NSArray
+                    
+                    for object in resultsArray {
+                        if let jsonObject = object as? NSDictionary{
+                            self.films.append(jsonObject["title"] as! String)
+                        }
+                    }
+                }
+                
+            }
+            
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+            
+        }catch {
+            print(error)
+        }
+    })
+}
+
+    
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return films.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: "GETFilms_TableViewController"), for: indexPath)
+        
+        cell.textLabel?.text = films[indexPath.row]
+        
+        return cell
+    }
+    
+    
+}
